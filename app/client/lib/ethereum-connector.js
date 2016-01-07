e0111fd1a00a5a00ad9a81b7a947f5e4ae7a93ed
Laurentiu Andronache <laurentiu.andronache@trailung.ro>
2016-01-07 16:29:48 +0000

    I've done the modifications that you requested.

--- app/client/lib/ethereum-connector.js
+++ app/client/lib/ethereum-connector.js
@@ -2,6 +2,3 @@
 if (typeof web3 == "undefined") {
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
-else if (!web3.currentProvider)
-    web3.setProvider(new Web3.providers.HttpProvider("http://localhost:8545"));
-
